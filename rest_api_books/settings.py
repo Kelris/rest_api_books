52243ca67d149ad3b169cc305d3fe6eef8baef83
Katarzyna Zych <katarzyna.zych.91@gmail.com>
2020-10-19 11:20:04 +0200

    heroku settings

--- rest_api_books/settings.py
+++ rest_api_books/settings.py
@@ -78,12 +78,7 @@ WSGI_APPLICATION = 'rest_api_books.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
